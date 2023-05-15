4deb26db234f3d088da6bdf172cceacc48ef9c3d
Franz <franz.tapia-chaca@outlook.com>
2023-05-15 14:57:26 +0200

    finished seg score analysis

--- Experiments-with-OrganoTrack/comparing-seg-accuracy-bw-methods-on-diff-datasets.py
+++ Experiments-with-OrganoTrack/comparing-seg-accuracy-bw-methods-on-diff-datasets.py
@@ -19,70 +19,6 @@ def GetDatasetsDirs(datasets):
                    'OrganoID-Original': Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData')}
     return datasetDirs[datasets]
 
-    # EMC dataset - segmented by OrganoTrack
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-    # exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-    # set1_pred_Dir = exportPath / 'segmented'
-    # segmenter = 'OrganoTrack'
-    # datasetName = 'EMC'
-
-    # EMC dataset - segmented by OrganoID
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-    # exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-    # set1_pred_Dir = exportPath / 'OrganoID_segmented'
-    # segmenter = 'OrganoID'
-    # datasetName = 'EMC'
-
-    # # EMC dataset - segmented by SAM
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-    # exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-    # set1_pred_Dir = exportPath / 'OrganoID_segmented'
-    # segmenter = 'OrganoID'
-    # datasetName = 'EMC'
-
-    # # EMC dataset - segmented by Harmony
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-    # exportPath = Path('/home/franz/Documents/mep/data/experiments/220405-Cis-drug-screen/Harmony-masks-with-analysis-220318-106TP24-15BME-CisGemCarbo-v4/images-for-OrganoTrack-Harmony-comparison/export')
-    # set1_pred_Dir = exportPath / 'OrganoID_segmented'
-    # segmenter = 'OrganoID'
-    # datasetName = 'EMC'
-
-    # OrganoID Gemcitabine dataset - segmented by OrganoTrack
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
-    # exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
-    # set1_pred_Dir = exportPath / 'segmented'
-    # segmenter = 'OrganoTrack'
-    # datasetName = 'OrganoID-OriginalData'
-
-    # OrganoID Gemcitabine dataset - segmented by OrganoID
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
-    # exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
-    # set1_pred_Dir = exportPath / 'OrganoID-segmented'
-    # segmenter = 'OrganoID'
-    # datasetName = 'OrganoID-OriginalData'
-
-    # # OrganoID MouseOrganoids dataset - segmented by OrganoTrack
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
-    # exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
-    # set1_pred_Dir = exportPath / 'segmented'
-    # segmenter = 'OrganoTrack'
-    # datasetName = 'OrganoID-MouseOrganoids'
-
-    # # OrganoID MouseOrganoids dataset - segmented by OrganoID
-    # set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
-    # set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
-    # exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
-    # set1_pred_Dir = exportPath / 'OrganoID-segmented'
-    # segmenter = 'OrganoID'
-    # datasetName = 'OrganoID-MouseOrganoids'
-
 def LoadImages(datasetsDirs, predictionMethods):
 
     datasetsGroundTruthsAndPredictions = dict()
@@ -113,39 +49,63 @@ def LoadImages(datasetsDirs, predictionMethods):
 
     return datasetsGroundTruthsAndPredictions
 
-def CalculatePredictionAccuracies(datasetsGtAndPreds):
-    # Create directory to store overlays
-    overlayExportPath = exportPath / (segmenter + '-overlay')
-    if not os.path.exists(overlayExportPath):
-        os.mkdir(overlayExportPath)
+def CalculatePredictionScores(datasetsGtAndPreds, datasetsDirs, predictionMethods):
 
-    segmentationScores = np.zeros((len(predictionImages), 3))
-    # Evaluating prediction against ground truth
-    for i, (prediction, groundTruth) in enumerate(zip(predictionImages, groundTruthImages)):
-        segmentationScores[i], overlay = EvaluateSegmentationAccuracy(prediction, groundTruth)
-        SaveOverlay(overlay, overlayExportPath, imageNames[i])
+    datasetsSegScoresWithDiffMethods = dict()
 
+    for dataset in list(datasetsDirs.keys()):
+        segScoresWithDiffMethods = dict()
+        datasetDir = datasetsDirs[dataset]
+        groundTruthImages = datasetsGtAndPreds[dataset]['groundTruth'][0]
 
-def ExportPredictionAccuracies():
-    # Exportnig segmentation accuracies
-    indexNames = []
-    for i in range(len(imageNames)):
-        indexNames.append(imageNames[i].name)
+        for method in predictionMethods:
+            predictedImages = datasetsGtAndPreds[dataset][method][0]
+            predictedImagesNames = datasetsGtAndPreds[dataset][method][1]
 
-    df = pd.DataFrame(segmentationScores, index=indexNames, columns=['f1', 'iou', 'dice'])
+            # Create directory to store overlays
+            exportPath = datasetDir / 'predictions'
+            overlayExportPath = exportPath / (method + '-overlay')
+            if not os.path.exists(overlayExportPath):
+                os.mkdir(overlayExportPath)
 
-    outputPath = exportPath / (segmenter + '-seg-scores-' + datasetName + '.xlsx')
+            segmentationScores = np.zeros((len(groundTruthImages), 3))
 
-    with pd.ExcelWriter(str(outputPath.absolute())) as writer:
-        df.to_excel(writer, sheet_name=datasetName, startrow=1)
-        # Within .to_excel(), startrow/col are 0-indexed. Startcol calculated to fit df's next to each other
+            # Evaluating prediction against ground truth
+            for i, (prediction, groundTruth) in enumerate(zip(predictedImages, groundTruthImages)):
+                segmentationScores[i], overlay = EvaluateSegmentationAccuracy(prediction, groundTruth)
+                SaveOverlay(overlay, overlayExportPath, predictedImagesNames[i])
 
+            segScoresWithDiffMethods[method] = segmentationScores
+            segScoresWithDiffMethods['imageNames'] = predictedImagesNames
 
+        datasetsSegScoresWithDiffMethods[dataset] = segScoresWithDiffMethods
+
+def ExportPredictionScores(datasetsPredictionScores, analysisFileName, predictionMethods):
+    # Exporting segmentation accuracies
+
+    datasets = list(datasetsPredictionScores.keys())
+
+    with pd.ExcelWriter(str(analysisFileName.absolute())) as writer:
+
+        for dataset in datasets:
+
+            for j, method in enumerate(predictionMethods):
+                segmentationScores = datasetsPredictionScores[dataset][method]
+                indexNames = []
+                imageNames = datasetsPredictionScores[dataset]['imageNames']
+                for i in range(len(imageNames)):
+                    indexNames.append(imageNames[i].name)
+
+                df = pd.DataFrame(segmentationScores, index=indexNames, columns=['f1', 'iou', 'dice'])
+                df.to_excel(writer, sheet_name=dataset, startrow=1, startcol=j * (3 + 2))
+                # Within .to_excel(), startrow/col are 0-indexed. Startcol calculated to fit df's next to each other
+
+def LoadPredictionScoreAnalysis():
+    pass
 
 
 def PlotPredictionAccuracies():
     pass
-
 
 def OrganoTrackVsHarmony():  # one dataset
     datasets = ['EMC-preliminary']
@@ -157,14 +117,84 @@ def OrganoTrackVsHarmony():  # one dataset
     if not os.path.exists(analysisFile):
         datasetsDirs = GetDatasetsDirs(datasets)
         datasetsGtAndPreds = LoadImages(datasetsDirs, predictors)
-        datasetsPredictionAccuracies = CalculatePredictionAccuracies(datasetsGtAndPreds)
-        ExportPredictionAccuracies()
+        datasetsPredictionScores = CalculatePredictionScores(datasetsGtAndPreds, datasetsDirs, predictors)
+        ExportPredictionScores(datasetsPredictionScores, analysisFile, predictors)
     else:
-        dataSetDirToLoad = GetDataSetDirToLoad()
-        LoadPredictionAccuracies()
+        ExperimentPredictionScores = LoadPredictionScoreAnalysis(analysisFile)
 
     PlotPredictionAccuracies()
 
 
 if __name__ == '__main__':
     OrganoTrackVsHarmony()
+
+
+
+
+
+'''
+    Previous directories - ignore
+'''
+# EMC dataset - segmented by OrganoTrack
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
+# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
+# set1_pred_Dir = exportPath / 'segmented'
+# segmenter = 'OrganoTrack'
+# datasetName = 'EMC'
+
+# EMC dataset - segmented by OrganoID
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
+# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
+# set1_pred_Dir = exportPath / 'OrganoID_segmented'
+# segmenter = 'OrganoID'
+# datasetName = 'EMC'
+
+# # EMC dataset - segmented by SAM
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
+# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
+# set1_pred_Dir = exportPath / 'OrganoID_segmented'
+# segmenter = 'OrganoID'
+# datasetName = 'EMC'
+
+# # EMC dataset - segmented by Harmony
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
+# exportPath = Path('/home/franz/Documents/mep/data/experiments/220405-Cis-drug-screen/Harmony-masks-with-analysis-220318-106TP24-15BME-CisGemCarbo-v4/images-for-OrganoTrack-Harmony-comparison/export')
+# set1_pred_Dir = exportPath / 'OrganoID_segmented'
+# segmenter = 'OrganoID'
+# datasetName = 'EMC'
+
+# OrganoID Gemcitabine dataset - segmented by OrganoTrack
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
+# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
+# set1_pred_Dir = exportPath / 'segmented'
+# segmenter = 'OrganoTrack'
+# datasetName = 'OrganoID-OriginalData'
+
+# OrganoID Gemcitabine dataset - segmented by OrganoID
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
+# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
+# set1_pred_Dir = exportPath / 'OrganoID-segmented'
+# segmenter = 'OrganoID'
+# datasetName = 'OrganoID-OriginalData'
+
+# # OrganoID MouseOrganoids dataset - segmented by OrganoTrack
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
+# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
+# set1_pred_Dir = exportPath / 'segmented'
+# segmenter = 'OrganoTrack'
+# datasetName = 'OrganoID-MouseOrganoids'
+
+# # OrganoID MouseOrganoids dataset - segmented by OrganoID
+# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
+# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
+# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
+# set1_pred_Dir = exportPath / 'OrganoID-segmented'
+# segmenter = 'OrganoID'
+# datasetName = 'OrganoID-MouseOrganoids'
