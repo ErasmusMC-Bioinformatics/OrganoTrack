b03304d9e1966c6eb7c667b4950901744ae13ad2
Franz <franz.tapia-chaca@outlook.com>
2023-05-17 17:23:08 +0200

    created loader ader of predictions

--- Experiments-with-OrganoTrack/comparing-seg-accuracy-bw-methods-on-diff-datasets.py
+++ Experiments-with-OrganoTrack/comparing-seg-accuracy-bw-methods-on-diff-datasets.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import openpyxl
 
 '''
     With this file, boxplots can be generated, comparing the segmentation accuracies for different methods on different
@@ -106,7 +107,7 @@ def CalculatePredictionScores(datasetsGtAndPreds, datasetsDirs, predictionMethod
 
 def ExportPredictionScores(datasetsPredictionScores, analysisFileName, predictionMethods):
     # Exporting segmentation accuracies
-
+    measureCount = 3
     datasets = list(datasetsPredictionScores.keys())
 
     with pd.ExcelWriter(str(analysisFileName.absolute())) as writer:
@@ -114,46 +115,81 @@ def ExportPredictionScores(datasetsPredictionScores, analysisFileName, predictio
         for dataset in datasets:
 
             for j, method in enumerate(predictionMethods):
-                segmentationScores = datasetsPredictionScores[dataset][method]
-                indexNames = []
-                imageNames = datasetsPredictionScores[dataset]['imageNames']
-                for i in range(len(imageNames)):
-                    indexNames.append(imageNames[i].name)
+                methodSegmentationScores = datasetsPredictionScores[dataset][method]
 
-                df = pd.DataFrame(segmentationScores, index=indexNames, columns=['f1', 'iou', 'dice'])
-                df.to_excel(writer, sheet_name=dataset, startrow=1, startcol=j * (3 + 2))
+                indexNames = []
+                imagePaths = datasetsPredictionScores[dataset]['imageNames']
+                for i in range(len(imagePaths)):
+                    indexNames.append(imagePaths[i].name)
+
+                df = pd.DataFrame(methodSegmentationScores, index=indexNames, columns=['f1', 'iou', 'dice'])
+                df.to_excel(writer, sheet_name=dataset, startrow=1, startcol=j * (measureCount + 2))
                 # Within .to_excel(), startrow/col are 0-indexed. Startcol calculated to fit df's next to each other
 
-def LoadPredictionScoreAnalysis():
-    pass
+                writer.sheets[dataset].cell(row=1, column=j*(measureCount+2) + 1).value = str(method)
+                # Within .cell(), row and column are 1-indexed
+
+def LoadPredictionScoreAnalysis(analysisFilePath):
+    xls = openpyxl.load_workbook(analysisFilePath)
+    datasets = xls.sheetnames
+
+    datasetsPredictionScores = dict()
+    for dataset in datasets:
+
+        extension = analysisFilePath.suffix
+
+        if extension == '.xlsx':
+            oneDatasetMethodsPredictionsDf = pd.read_excel(analysisFilePath, header=None)
+
+        elif extension == '.csv':
+            oneDatasetMethodsPredictionsDf = pd.read_csv(analysisFilePath, header=None)
+
+        elif extension == '.tsv':
+            oneDatasetMethodsPredictionsDf = pd.read_csv(analysisFilePath, header=None, delimiter='\t')
+
+        oneDatasetMethodsPredictions = dict()
+
+
+        firstRow = oneDatasetMethodsPredictionsDf.iloc[0]
+        methods = firstRow.dropna().tolist()
+        methodIndeces = list(firstRow.notna()[firstRow.notna() == True].index)
+
+        for i, method in enumerate(methods):
+            methodPredictions = oneDatasetMethodsPredictionsDf.iloc[2:, methodIndeces[i]+1:methodIndeces[i]+4].to_numpy()
+            oneDatasetMethodsPredictions[method] = methodPredictions
+
+        datasetsPredictionScores[dataset] = oneDatasetMethodsPredictions
+
+        return datasetsPredictionScores
+
 
 
 def PlotPredictionAccuracies(datasetsPredictionScores, predictionMethods):
     datasets = list(datasetsPredictionScores.keys())
-    x = [1000]
+
     measures = ['F1', 'IOU', 'Dice']
     measureIndex = [0, 1, 2]
 
     plt.rcParams.update({'font.size': 15})
-    for measure, index in zip(measures, measureIndex):
+
+    for segAccuracyMeasure, index in zip(measures, measureIndex):
         fig, ax = plt.subplots()
-        for dataset in datasets:
-            for method in predictionMethods:
-                data.append(datasetsPredictionScores[dataset][method][:, index])
-                data = np.array(data).T
-                ax.boxplot(data, positions=x, showfliers=False) # one box plot corresponds to one method, not one dataset
-        ax.set_ylabel(f'{measure} score')
+
+        dataAcrossDatasetsAndMethods = []
+
+        for method in predictionMethods:
+            for dataset in datasets:
+                dataAcrossDatasetsAndMethods.append(datasetsPredictionScores[dataset][method][:, index])
+
+        # dataAcrossDatasetsAndMethods = np.array(dataAcrossDatasetsAndMethods).T
+        ax.boxplot(dataAcrossDatasetsAndMethods)  # one box plot corresponds to one method, not one dataset
+
+        ax.set_ylabel(f'{segAccuracyMeasure} score')
         ax.set_xlabel('Datasets')
         ax.set_ylim(0, 100)
-        ax.set_title(f'{measure} score') #  for OrganoTrack and the baseline on a sample of the EMC dataset
-        palette = ['b', 'g', 'r', 'c', 'm', 'k']
-        # for x, val, c in zip(xs, normFracGrowthValues, palette):
-        #     ax.scatter(x, val, alpha=0.4, color=c)
         plt.tight_layout()
         fig.show()
 
-
-    pass
 
 
 def OrganoTrackVsHarmony():  # one dataset
@@ -169,48 +205,47 @@ def OrganoTrackVsHarmony():  # one dataset
     # ViewLoadedImages(datasetsGtAndPreds)
 
     datasetsPredictionScores = CalculatePredictionScores(datasetsGtAndPreds, datasetsDirs, predictors)
-    #     ExportPredictionScores(datasetsPredictionScores, analysisFile, predictors)
+    # ExportPredictionScores(datasetsPredictionScores, analysisFile, predictors)
     # else:
-    #     ExperimentPredictionScores = LoadPredictionScoreAnalysis(analysisFile)
+    ExperimentPredictionScores = LoadPredictionScoreAnalysis(analysisFile)
 
-    # PlotPredictionAccuracies(datasetsPredictionScores, predictors)
+    PlotPredictionAccuracies(datasetsPredictionScores, predictors)
 
-    # Plotting
-    x = np.array([1000])
-    measures = ['F1', 'IOU', 'Dice']
-    measureIndex = [0, 1, 2]
-    boxWidth = 100
-    plt.rcParams.update({'font.size': 15})
-
-
-    for measure, index in zip(measures, measureIndex):
-        fig, ax = plt.subplots()
-        for dataset in datasets:
-            data1 = np.array(datasetsPredictionScores[dataset]['Harmony'][:, index]).T
-            ax.boxplot(data1, positions=x-100, showfliers=False, widths=boxWidth) # one box plot corresponds to one method, not one dataset
-            data2 = np.array(datasetsPredictionScores[dataset]['OrganoTrack'][:, index]).T
-            ax.boxplot(data2, positions=x+100,
-                       showfliers=False, widths=boxWidth)   # one box plot corresponds to one method, not one dataset
-            # fill with colors, https://matplotlib.org/stable/gallery/statistics/boxplot_color.html
-            # colors = ['lightblue', 'lightgreen']
-            # for bplot in bplot1:
-            #     for patch, color in zip(bplot['boxes'], colors):
-            #         patch.set_facecolor(color)
-
-        ax.set_ylabel(f'{measure} score')
-        ax.set_ylim(0, 100)
-        labels = [item.get_text() for item in ax.get_xticklabels()]
-        labels[0] = 'Baseline'  # change to OrganoTrack and baseline
-        labels[1] = 'OrganoTrack'
-        ax.set_xticklabels(labels)
-        ax.set_xlim(800, 1200)
-
-        ax.set_title(f'{measure} score') #  for OrganoTrack and the baseline on a sample of the EMC dataset
-        # palette = ['b', 'g', 'r', 'c', 'm', 'k']
-        # for x, val, c in zip(xs, normFracGrowthValues, palette):
-        #     ax.scatter(x, val, alpha=0.4, color=c)
-        plt.tight_layout()
-        fig.show()
+    # # Plotting
+    # x = np.array([1000])
+    # measures = ['F1', 'IOU', 'Dice']
+    # measureIndex = [0, 1, 2]
+    # boxWidth = 100
+    # plt.rcParams.update({'font.size': 15})
+    #
+    #
+    # for measure, index in zip(measures, measureIndex):
+    #     fig, ax = plt.subplots()
+    #     for dataset in datasets:
+    #         data1 = np.array(datasetsPredictionScores[dataset]['Harmony'][:, index]).T
+    #         ax.boxplot(data1, positions=x-100, showfliers=False, widths=boxWidth) # one box plot corresponds to one method, not one dataset
+    #         data2 = np.array(datasetsPredictionScores[dataset]['OrganoTrack'][:, index]).T
+    #         ax.boxplot(data2, positions=x+100,
+    #                    showfliers=False, widths=boxWidth)   # one box plot corresponds to one method, not one dataset
+    #         # fill with colors, https://matplotlib.org/stable/gallery/statistics/boxplot_color.html
+    #         # colors = ['lightblue', 'lightgreen']
+    #         # for bplot in bplot1:
+    #         #     for patch, color in zip(bplot['boxes'], colors):
+    #         #         patch.set_facecolor(color)
+    #
+    #     ax.set_ylabel(f'{measure} score')
+    #     ax.set_ylim(0, 100)
+    #     labels = [item.get_text() for item in ax.get_xticklabels()]
+    #     labels[0] = 'Baseline'  # change to OrganoTrack and baseline
+    #     labels[1] = 'OrganoTrack'
+    #     ax.set_xticklabels(labels)
+    #     ax.set_xlim(800, 1200)
+    #
+    #     # palette = ['b', 'g', 'r', 'c', 'm', 'k']
+    #     # for x, val, c in zip(xs, normFracGrowthValues, palette):
+    #     #     ax.scatter(x, val, alpha=0.4, color=c)
+    #     plt.tight_layout()
+    #     fig.show()
 
 def OrganoTrackVsOrganoID():  # one dataset
     datasets = ['EMC-preliminary', 'OrganoID-Mouse', 'OrganoID-Original']
@@ -230,122 +265,56 @@ def OrganoTrackVsOrganoID():  # one dataset
     # else:
     #     ExperimentPredictionScores = LoadPredictionScoreAnalysis(analysisFile)
 
-    # PlotPredictionAccuracies(datasetsPredictionScores, predictors)
+    PlotPredictionAccuracies(datasetsPredictionScores, predictors)
 
-    # Plotting
-    x = np.array([1000, 2000, 3000])  # 3 datasets
-    measures = ['F1', 'IOU', 'Dice']
-    measureIndex = [0, 1, 2]
-    boxWidth = 100
-    plt.rcParams.update({'font.size': 15})
-
-    # https://stackoverflow.com/questions/14952401/creating-double-boxplots-i-e-two-boxes-for-each-x-value
-    for measure, index in zip(measures, measureIndex):
-        fig, ax = plt.subplots()
-        data1 = []
-        data2 = []
-        for dataset in datasets:
-            data1.append(datasetsPredictionScores[dataset]['OrganoTrack'][:, index])
-            data2.append(datasetsPredictionScores[dataset]['OrganoID'][:, index])
-        ax.boxplot(data1, positions=x-100, showfliers=False, widths=boxWidth) # one box plot corresponds to one method, not one dataset
-        ax.boxplot(data2, positions=x+100,
-                       showfliers=False, widths=boxWidth)   # one box plot corresponds to one method, not one dataset
-            # fill with colors, https://matplotlib.org/stable/gallery/statistics/boxplot_color.html
-            # colors = ['lightblue', 'lightgreen']
-            # for bplot in bplot1:
-            #     for patch, color in zip(bplot['boxes'], colors):
-            #         patch.set_facecolor(color)
-
-        ax.set_ylabel(f'{measure} score')
-        ax.set_ylim(0, 100)
-        plt.xticks(x)
-        labels = [item.get_text() for item in ax.get_xticklabels()]
-        labels[0] = 'EMC\npreliminary\ndataset'
-        labels[1] = 'OrganoID\nmouse organoids\ndataset'
-        labels[2] = 'OrganoID\noriginal organoids\ndataset'
-
-        ax.set_xticklabels(labels)
-        ax.set_xlim(800, 3200)
-
-        ax.set_title(f'{measure} score') #  for OrganoTrack and the baseline on a sample of the EMC dataset
-        # palette = ['b', 'g', 'r', 'c', 'm', 'k']
-        # for x, val, c in zip(xs, normFracGrowthValues, palette):
-        #     ax.scatter(x, val, alpha=0.4, color=c)
-        plt.tight_layout()
-        fig.show()
-        print('h')
+    # # Plotting
+    # x = np.array([1000, 2000, 3000])  # 3 datasets
+    # measures = ['F1', 'IOU', 'Dice']
+    # measureIndex = [0, 1, 2]
+    # boxWidth = 100
+    # plt.rcParams.update({'font.size': 15})
+    #
+    # # https://stackoverflow.com/questions/14952401/creating-double-boxplots-i-e-two-boxes-for-each-x-value
+    # for measure, index in zip(measures, measureIndex):
+    #     fig, ax = plt.subplots()
+    #     data1 = []
+    #     data2 = []
+    #     for dataset in datasets:
+    #         data1.append(datasetsPredictionScores[dataset]['OrganoTrack'][:, index])
+    #         data2.append(datasetsPredictionScores[dataset]['OrganoID'][:, index])
+    #     ax.boxplot(data1, positions=x-100, showfliers=False, widths=boxWidth) # one box plot corresponds to one method, not one dataset
+    #     ax.boxplot(data2, positions=x+100,
+    #                    showfliers=False, widths=boxWidth)   # one box plot corresponds to one method, not one dataset
+    #         # fill with colors, https://matplotlib.org/stable/gallery/statistics/boxplot_color.html
+    #         # colors = ['lightblue', 'lightgreen']
+    #         # for bplot in bplot1:
+    #         #     for patch, color in zip(bplot['boxes'], colors):
+    #         #         patch.set_facecolor(color)
+    #
+    #     ax.set_ylabel(f'{measure} score')
+    #     ax.set_ylim(0, 100)
+    #     plt.xticks(x)
+    #     labels = [item.get_text() for item in ax.get_xticklabels()]
+    #     labels[0] = 'EMC\npreliminary\ndataset'
+    #     labels[1] = 'OrganoID\nmouse organoids\ndataset'
+    #     labels[2] = 'OrganoID\noriginal organoids\ndataset'
+    #
+    #     ax.set_xticklabels(labels)
+    #     ax.set_xlim(800, 3200)
+    #
+    #     ax.set_title(f'{measure} score') #  for OrganoTrack and the baseline on a sample of the EMC dataset
+    #     palette = ['b', 'g', 'r', 'c', 'm', 'k']
+    #     # for i in range(len(conditionsNewOrder)):
+    #     #     xs.append(np.random.normal(i + 1, 0.04, areaDFsSorted[i]['Norm Frac Growth'].values.shape[0]))
+    #     #
+    #     # for x, val, c in zip(xs, normFracGrowthValues, palette):
+    #     #     ax.scatter(x, val, alpha=0.4, color=c)
+    #     plt.tight_layout()
+    #     fig.show()
+    #     print('h')
 
 
 if __name__ == '__main__':
-    OrganoTrackVsOrganoID()
+    OrganoTrackVsHarmony()
 
 
-
-
-
-'''
-    Previous directories - ignore
-'''
-# EMC dataset - segmented by OrganoTrack
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-# set1_pred_Dir = exportPath / 'segmented'
-# segmenter = 'OrganoTrack'
-# datasetName = 'EMC'
-
-# EMC dataset - segmented by OrganoID
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-# set1_pred_Dir = exportPath / 'OrganoID_segmented'
-# segmenter = 'OrganoID'
-# datasetName = 'EMC'
-
-# # EMC dataset - segmented by SAM
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-# exportPath = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/predictions')
-# set1_pred_Dir = exportPath / 'OrganoID_segmented'
-# segmenter = 'OrganoID'
-# datasetName = 'EMC'
-
-# # EMC dataset - segmented by Harmony
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/annotated/annotations')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/for-creating-OrganoTrack/training-dataset/preliminary-gt-dataset/2.images-with-edited-names-finished-annotating')
-# exportPath = Path('/home/franz/Documents/mep/data/experiments/220405-Cis-drug-screen/Harmony-masks-with-analysis-220318-106TP24-15BME-CisGemCarbo-v4/images-for-OrganoTrack-Harmony-comparison/export')
-# set1_pred_Dir = exportPath / 'OrganoID_segmented'
-# segmenter = 'OrganoID'
-# datasetName = 'EMC'
-
-# OrganoID Gemcitabine dataset - segmented by OrganoTrack
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
-# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
-# set1_pred_Dir = exportPath / 'segmented'
-# segmenter = 'OrganoTrack'
-# datasetName = 'OrganoID-OriginalData'
-
-# OrganoID Gemcitabine dataset - segmented by OrganoID
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/groundTruth')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/original')
-# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/OriginalData/export')
-# set1_pred_Dir = exportPath / 'OrganoID-segmented'
-# segmenter = 'OrganoID'
-# datasetName = 'OrganoID-OriginalData'
-
-# # OrganoID MouseOrganoids dataset - segmented by OrganoTrack
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
-# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
-# set1_pred_Dir = exportPath / 'segmented'
-# segmenter = 'OrganoTrack'
-# datasetName = 'OrganoID-MouseOrganoids'
-
-# # OrganoID MouseOrganoids dataset - segmented by OrganoID
-# set1_GT_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/GroundTruth')
-# set1_ori_Dir = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Original')
-# exportPath = Path('/home/franz/Documents/mep/data/published-data/OrganoID-data/combinedForOrganoTrackTesting/MouseOrganoids/Export')
-# set1_pred_Dir = exportPath / 'OrganoID-segmented'
-# segmenter = 'OrganoID'
-# datasetName = 'OrganoID-MouseOrganoids'
