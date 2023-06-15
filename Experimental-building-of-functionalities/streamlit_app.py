24f128843d08b42c30d58b8bc1d088e9e98d408f
Franz <franz.tapia-chaca@outlook.com>
2023-06-15 17:44:08 +0200

    understood show_aowanns

--- Experimental-building-of-functionalities/streamlit_app.py
+++ Experimental-building-of-functionalities/streamlit_app.py
@@ -5,62 +5,64 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
-modelCheckpoints = {'vit_b': '/home/franz/OrganoTrack/Experimental-building-of-functionalities/sam_vit_b_01ec64.pth',
+modelCheckpoints = {'vit_b': 'Experimental-building-of-functionalities/sam_vit_b_01ec64.pth',
                     'vit_h': 'sam_vit_h_4b8939.pth',
                     'vit_l': 'sam_vit_l_0b3195.pth'}
 
 
 
-def show_anns(anns):
-    if len(anns) == 0:
+def show_anns(objectsSegmentedBySAM):
+    if len(objectsSegmentedBySAM) == 0:
         return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    sortedSAMobjects = sorted(objectsSegmentedBySAM, key=(lambda x: x['area']), reverse=True)  # in decreasing area
+
     ax = plt.gca()
     ax.set_autoscale_on(False)
 
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    return img
-    # ax.imshow(img)
+    imgHeight = sortedSAMobjects[0]['segmentation'].shape[0]  # look at just the first object
+    imgWidth = sortedSAMobjects[0]['segmentation'].shape[1]
+    colorObjectMasks = np.ones((imgHeight, imgWidth, 4))  # 3D array, height x width x 4. i.e. a stack of 4 images
+
+    colorObjectMasks[:, :, 3] = 0              # the last image in the stack = 0
+    for samObject in sortedSAMobjects:     #
+        samObjectSegmentataionArray = samObject['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])  # [ 0-1 , 0-1 , 0-1 , 0.35 ]
+        colorObjectMasks[samObjectSegmentataionArray] = color_mask
+    return colorObjectMasks
+
 
 def SegmentBySAM(image, model, modelCheckpoint):
     sam = sam_model_registry[model](checkpoint=modelCheckpoint)
+    sam.to(device='cuda')
     mask_generator = SamAutomaticMaskGenerator(sam)
     return mask_generator.generate(image)
 
 
 def main_loop():
-    st.title("Sam Annotator. hello!")
-    st.subheader("This app allows you to segment an image and generate a binary image with SAM")
-    st.text("We use OpenCV, the Segment Anythin Model, and Streamlit for this demo")
-
-    # blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    # brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    # apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+    st.title("SAM Annotator")
 
     image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg', 'tiff', 'tif'])
     if not image_file:
         return None
 
-
     original_image = Image.open(image_file)
     original_image = np.array(original_image)
-    st.markdown('Type of image before uint8 cnoversion: ' + str(type(original_image)))
-    st.markdown('Printing the array: '+ str((original_image)))
-    st.markdown('Getting the image shape: ' + str(np.shape(original_image)))
 
     original_image = (original_image / 256).astype('uint8')
-    st.markdown('Type of image after uint8 cnoversion: '  + str(type(original_image)))
-    st.markdown('Printing the array: '+ str((original_image)))
-    st.markdown('Getting the image shape: ' + str(np.shape(original_image)))
     model = list(modelCheckpoints.keys())[0]
 
     image_file = cv.cvtColor(original_image, cv.COLOR_GRAY2RGB)
     mask = SegmentBySAM(image_file, model, modelCheckpoints[model]) # SAM expects a 3 channel image
+    # mask = list of dicts. Each dict element belongs to one segmented object
+    # mask[0] = {'segmentation': array,
+    #            'area': e.g. 1539
+    #            'bbox': e.g. [0, 736, 51, 36]
+    #            'predicted_iou': e.g. 1.0169870853424072
+    #            'point_coords': e.g. [[16.875, 759.375]]
+    #            'stability_score': e.g. 0.9896439909934998
+    #            'crop_box': e.g. [0, 0, 1080, 1080]
+
+    # mask[0]['segmentation'] = boolean array of object, array with dimensions of image
 
     plt.figure(figsize=(20, 20))
     plt.imshow(image_file)
@@ -68,15 +70,10 @@ def main_loop():
     plt.axis('off')
     plt.show()
 
-    # processed_image = blur_image(original_image, blur_rate)
-    # processed_image = brighten_image(processed_image, brightness_amount)
-    #
-    # if apply_enhancement_filter:
-    #     processed_image = enhance_details(processed_image)
-
     st.text("Original Image vs Processed Image")
-    st.image([original_image, output])
+    st.image([original_image, output], width=600)
 
 
 if __name__ == '__main__':
     main_loop()
+
